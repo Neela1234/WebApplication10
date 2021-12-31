b2d0a40b3cf6f2a105f7a045f2fbd33a01e4accc
home <home@WINDOWS-PQ21EPG>
2021-12-31 16:39:54 +0530

    Add project files.

---
+++ WebApplication10/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication10.Models;
+
+namespace WebApplication10.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Customer> customers = new List<Customer>();
+            {
+                customers.Add(new Customer() { CustomerID = 34, CustomerName = "neela", CustomerAddresss = "khammam" });
+                customers.Add(new Customer() { CustomerID = 678, CustomerName = "krishna", CustomerAddresss = "hyderabad" });
+                customers.Add(new Customer() {CustomerID = 456, CustomerName = "kousik", CustomerAddresss = "vijayawada" });
+                ViewData["customers"] = customers;
+
+
+            }
+
+            return View();
+        }
+    }
+}
